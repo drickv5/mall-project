5c0a477fb23c5cb564eee7901ee0507cf57a7a70
qiaoguojun <504599399@qq.com>
2017-01-04 17:03:29 +0800

    用户模型类

--- mall/user_qiao/models.py
+++ mall/user_qiao/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    id = models.AutoField
+    uname=models.CharField(max_length=40)#用户名
+    upwd=models.CharField(max_length=50)#密码
+    isDelete=models.BooleanField(default=False)#逻辑删除
+    recipients=models.CharField(max_length=40)#收件人
+    address=models.CharField(max_length=60)#收货地址
+    phone=models.CharField(max_length=20)#联系电话
+
