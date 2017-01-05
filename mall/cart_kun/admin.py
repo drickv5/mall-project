11d615b90966f1a906a5996876fd3da1d21fc0c2
xwp <python_d@sina.com>
2017-01-05 17:21:47 +0800

    与数据库交互

--- mall/cart_kun/admin.py
+++ mall/cart_kun/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import *
+admin.site.register(CartInfo)
+admin.site.register(OrderDtailInfo)
+admin.site.register(OrderInfo)
 
 # Register your models here.
+
+
+
+
