4c47a5a54af95b15fdace852916e52d32ea520bf
qiaoguojun <504599399@qq.com>
2017-01-11 19:16:24 +0800

    tijiao

--- mall/cart_kun/models.py
+++ mall/cart_kun/models.py
@@ -1,23 +1,24 @@
-#encoding=utf-8
-from django.db import models
-from goods_qi.models import GoodsInfo
-from user_qiao.models import UserInfo
-
-
-# Create your models here.
-class CartInfo(models.Model):
-    user = models.ForeignKey(UserInfo)#引入用户外键
-    goods = models.ForeignKey(GoodsInfo)#引入商品外键
-    count = models.IntegerField()#商品数量
-
-class OrderInfo(models.Model):
-    user = models.ForeignKey(UserInfo)#引入用户外键
-    ototal = models.DecimalField(max_digits=7,decimal_places=2)#订单总价格
-    state = models.BooleanField(default=0)#状态默认‘未支付’
-
-class OrderDtailInfo(models.Model):
-    order = models.ForeignKey(OrderInfo)#用表单外键做订单号
-    goods = models.ForeignKey(GoodsInfo)#引用商品列表
-    count = models.IntegerField()#商品数量
-    price = models.DecimalField(max_digits=7,decimal_places=2)#商品单价
-
+# #encoding=utf-8
+# from django.db import models
+# from goods_qi.models import GoodsInfo
+# from user_qiao.models import UserInfo
+#
+#
+# # Create your models here.
+# class CartInfo(models.Model):
+#     user = models.ForeignKey(UserInfo)#引入用户外键
+#     goods = models.ForeignKey(GoodsInfo)#引入商品外键
+#     count = models.IntegerField()#商品数量
+#
+# class OrderInfo(models.Model):
+#     user = models.ForeignKey(UserInfo)#引入用户外键
+#     ototal = models.DecimalField(max_digits=7,decimal_places=2)#订单总价格
+#     state = models.BooleanField(default=0)#状态默认‘未支付’
+#
+# #订单号　订单日期
+# class OrderDtailInfo(models.Model):
+#     order = models.ForeignKey(OrderInfo)#用表单外键做订单号
+#     goods = models.ForeignKey(GoodsInfo)#引用商品列表
+#     count = models.IntegerField()#商品数量
+#     price = models.DecimalField(max_digits=7,decimal_places=2)#商品单价ssssssssssssssssssssssssssss
+#
--- mall/cart_kun/views.py
+++ mall/cart_kun/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from models import *
-
-# Create your views here.
-
-def cartList(request):
-    list = CartInfo.objects.filter(user_id=1)
-    context = {'list':list}
-    return render(request,'cart.html',context)
+# from django.shortcuts import render
+# from models import *
+#
+# # Create your views here.
+#
+# def cartList(request):
+#     list = CartInfo.objects.filter(user_id=1)
+#     context = {'list':list}
+#     return render(request,'cart.html',context)
